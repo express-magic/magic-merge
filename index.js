150c55b5fb9ac3c2f694e3fc3e1cbdabdbefa7ce
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-29 04:55:55 +0200

    removed dependencies

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-"use strict";
-
-var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
-
 /*
 * Recursively merge properties of two objects 
 */
+"use strict";
+
 exports.merge = merge;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var each = require("magic-loops").each;
-
-var is = _interopRequire(require("is"));
-
 function merge(obj1, obj2) {
-  if (is.array(obj1) && is.array(obj2)) {
+  if (obj1 && typeof obj1.concat === "function" && obj2) {
     return obj1.concat(obj2);
   }
-  each(obj2, function (obj, p) {
-    try {
-      // Property in destination object set; update its value.
-      if (is.a(obj, "object")) {
-        obj1[p] = merge(obj1[p], obj);
-      } else {
+  for (var p in obj2) {
+    if (obj2.hasOwnProperty(p)) {
+      var obj = obj2[p];
+      try {
+        // Property in destination object set; update its value.
+        if (typeof obj === "object") {
+          obj1[p] = merge(obj1[p], obj);
+        } else {
+          obj1[p] = obj;
+        }
+      } catch (e) {
+        // Property in destination object not set; create it and set its value.
         obj1[p] = obj;
       }
-    } catch (e) {
-      // Property in destination object not set; create it and set its value.
-      obj1[p] = obj;
     }
-  });
+  }
   return obj1;
 }
 
-exports["default"] = merge;
+var mergeObjects = merge;
 
-//# sourceMappingURL=index.js.map
+exports.mergeObjects = mergeObjects;
+exports["default"] = merge;
--- src/merge.js
+++ src/merge.js
@@ -1,27 +1,29 @@
-import {each} from 'magic-loops';
-import is from 'is';
-
 /*
 * Recursively merge properties of two objects 
 */
 export function merge(obj1, obj2) {
-  if ( is.array(obj1) && is.array(obj2) ) {
+  if ( obj1 && typeof obj1.concat === 'function' && obj2) {
     return obj1.concat(obj2);
   }
-  each(obj2, (obj, p) => {
-    try {
-      // Property in destination object set; update its value.
-      if ( is.a(obj, 'object') ) {
-        obj1[p] = merge(obj1[p], obj);
-      } else {
+  for( let p in obj2 ) {
+    if ( obj2.hasOwnProperty(p) ) {
+      let obj = obj2[p];
+      try {
+        // Property in destination object set; update its value.
+        if ( typeof obj === 'object' ) {
+          obj1[p] = merge(obj1[p], obj);
+        } else {
+          obj1[p] = obj;
+        }
+      } catch(e) {
+        // Property in destination object not set; create it and set its value.
         obj1[p] = obj;
       }
-    } catch(e) {
-      // Property in destination object not set; create it and set its value.
-      obj1[p] = obj;
     }
-  });
+  }
   return obj1;
 }
+
+export var mergeObjects = merge;
 
 export default merge;
